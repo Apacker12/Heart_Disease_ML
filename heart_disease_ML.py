ad3d7de7444d734afd547be59b8f2bf225413723
Apacker12 <pachura@sunypoly.edu>
2025-10-13 18:02:38 -0400

    Update heart_disease_ML.py

--- heart_disease_ML.py
+++ heart_disease_ML.py
@@ -1,7 +1,5 @@
 # Importing libraries
-import streamlit as st
 import numpy as np
-import joblib
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
